88c3f7efacca7476579a3cbd25bc8f9fd5cc3e2b
Akash S <akashspalloor@gmail.com>
2024-11-03 12:50:07 +0530

    designing over

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//Create a website to find the grade of the mark given by the student in the input,
+//  and on clicking the submit button, display whether the student is passed or not and their grade.
+//   Grade Description:
+//    A+ (90-100) 
+//    A (80-89) 
+//    B+ (70-79) 
+//    B (60-69) 
+//    C+ (50-59) 
+//    C (40-49) 
+//    D+ (30-39) 
+//    D (20-29) 
+//    E (Below 20) 
+//    Minimum passing grade: D+ (30-39)
